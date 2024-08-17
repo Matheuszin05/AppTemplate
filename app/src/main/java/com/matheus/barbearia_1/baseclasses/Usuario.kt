a39af8ef6882507b99768548586956fef52af3c9
Matheus sá <matpedrososa@gmail.com>
2024-08-16 21:59:47 -0300

    Atualização

---
+++ app/src/main/java/com/matheus/barbearia_1/baseclasses/Usuario.kt
@@ -0,0 +1,8 @@
+package com.matheus.barbearia_1.baseclasses
+
+data class Usuario(
+    var key: String? = null,
+    var nome: String? = null,
+    var email: String? = null,
+    var password: String? = null
+)
